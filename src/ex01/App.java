36441f81c5fa1d22a0d526ceb35c45ddf4dc4620
KimNaKim <shila66@naver.com>
2026-02-10 10:45:59 +0900

    전략  패턴

--- src/ex01/App.java
+++ src/ex01/App.java
@@ -1,7 +1,7 @@
 package ex01;
 
 // SOLID : 객체 지향 원칙
-// OCP : SOLID의 O - 새로운 코드 추가는 가능하되(OPEN), 기존 코드 수정은 금지되어야(CLOSE) 한다.
+// OCP : SOLID의 O - 새로운 코드 추가는 가능하되(OPEN), 기존의 완성된 코드 수정은 금지되어야(CLOSE) 한다.
 // DIP : SOLID의 D - 추상적인 것에 의존하라.
 public class App {
     public static void main(String[] args) {
