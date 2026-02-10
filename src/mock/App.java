42a49d9f5752552919afbe5a4aff0b9d87bc092c
KimNaKim <shila66@naver.com>
2026-02-10 14:27:20 +0900

    adaptor_mock

--- src/mock/App.java
+++ src/mock/App.java
@@ -1,7 +1,7 @@
 package mock;
 
-
-
+//필요한 객체가 아직 만들어지지 않았다면 Mock(가짜 객체)를 구현해둘 것
+//후에 만들어진 객체와 연결할 때에는 Adaptor 이용
 public class App {
     public static void main(String[] args) {
         MeterService ms = new MeterService(new MockMeter());
